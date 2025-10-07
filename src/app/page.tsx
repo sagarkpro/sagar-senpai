5228a8d8265028271fbe7e740fad2e2d6c5e0ac8
Sagar Khatri <sagarkhatri.pro@gmail.com>
2025-10-07 23:37:25 +0530

    portfolio init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,62 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+	return (
+		<div className="flex justify-center py-8">
+			<div className="w-[70rem]">
+				<div className="w-full flex gap-x-20">
+					<div className="w-[31rem] h-[47rem] shrink-0 relative rounded-[3rem] overflow-clip group">
+						<Image className="object-cover transition-transform duration-500 ease-in-out group-hover:scale-110" fill objectFit="cover" src="/sagar2.jpg" alt="sagar" />
+					</div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+					<div className="mt-1 flex flex-col gap-y-3 text-justify font-semibold text-lg">
+						<h1 className="text-3xl font-bold text-left">✨ Your Vibe Code Needed a Therapist, So I Showed Up ✨</h1>
+
+						<p>I have worked with some pretty cool startups like</p>
+
+						<p>
+							<span className="font-extrabold">ZentrumHub</span> - A Bootstrapped Startup revolutionizing the OTA (Online Travel Agency) Industry, Giving direct competition to big players like Booking.com, MakeMyTrip and recently recognized as the
+							Hot 25 Startup!
+						</p>
+
+						<p>
+							<span className="font-extrabold">Nexus1.ai</span> - An AI based data extraction tool. Automate your whole legacy data kept in PDFs and CSVs to transform them into useful entries in your own Datalake. (A very complex idea oversimplified)
+						</p>
+
+						<p className="text-justify font-semibold text-lg">
+							I clean other&apos;s <span className="font-extrabold">Vibe Code</span> mess-up, I write clean following the best practices I can find, I deliver end-to-end features. Be it Frontend, Backend or even Database, I got it covered!
+						</p>
+
+						<h1 className="mt-5 text-3xl font-bold text-left">My Expertise?</h1>
+
+						<p>
+							I specialize in <span className="font-bold">Java Spring Boot, React, Next.js, NoSQL (DynamoDb), SQL(PostgreSQL)</span>
+						</p>
+
+						<p>
+							<span className="font-bold text-xl">&bull;Java</span> - A little verbose but the most reliable language out there.
+						</p>
+					</div>
+				</div>
+
+				<div className="w-full mt-5 flex flex-col gap-y-3 text-justify font-semibold text-lg">
+					<p>
+						{" "}
+						<span className="font-bold text-xl">&bull;Spring Boot</span> - Highly Opinionated, but hey, those opinions are from experts and I trust them. It&apos;s just like &quot;Plug & Play&quot;
+					</p>
+
+					<p>
+						{" "}
+						<span className="font-bold text-xl">&bull;Next.js</span> - With Next.js 15, you don&apos;t need to be an expert to make an highly optimizes and SEO friendly app, it offers everything you need out of the box! After all, this website is also made using
+						Next.js!
+					</p>
+
+					<p>
+						{" "}
+						<span className="font-bold text-xl">&bull;PostgreSQL & DynamoDb</span> - Do I even need to say anything? I mean where you gon&apos; save you data bro?
+					</p>
+				</div>
+			</div>
+		</div>
+	);
 }
