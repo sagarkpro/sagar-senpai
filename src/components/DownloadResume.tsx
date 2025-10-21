2d269b4501ae7b59e601e7b0007c919882d3f043
Sagar Khatri <sagarkhatri.pro@gmail.com>
2025-10-22 01:23:17 +0530

    even more black magic

--- src/components/DownloadResume.tsx
+++ src/components/DownloadResume.tsx
@@ -1,22 +1,25 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import React, { useEffect } from "react";
 
 export default function DownloadResume({ downloadResume = false }: Readonly<{ downloadResume: boolean }>) {
+  const router = useRouter();
+
 	function downloadFile(url: string, fileName: string) {
 		const link = document.createElement("a");
 		link.href = url;
 		link.download = fileName;
 		document.body.appendChild(link);
 		link.click();
-		document.body.removeChild(link);
+    link.remove();
 	}
 
 	useEffect(() => {
 		if (document && downloadResume) {
 			downloadFile("/Sagar_Khatri_Resume.pdf", "Sagar_Khatri_Resume.pdf");
 		}
-
+    router.replace("/");
 	}, [downloadResume]);
 
 	return <></>;
